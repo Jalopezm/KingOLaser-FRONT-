bd8a19f29ec8d6d7ed323457d1462ef698130e44
Jhota <2002juanan@gmail.com>
2023-06-15 13:21:45 +0200

    Commit

---
+++ KIng-o-Laser/nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Telegame'
+    }
+  },
+  srcDir:"./src",
+  ssr:false,
+  modules: [
+    "@nuxtjs/tailwindcss",
+    // With options
+    ["@nuxtjs/eslint-module", { lintOnStart: false }],
+  ],
+});
