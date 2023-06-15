21b1e4c7d0253bd2df51dd0b87d17a7587527676
Jhota <2002juanan@gmail.com>
2023-06-15 20:18:41 +0200

    Acessibility 100

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,10 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      title: 'Telegame'
+      title: 'Telegame',
+      htmlAttrs: {
+        lang: 'en'
+      },
     }
   },
   srcDir:"./src",
